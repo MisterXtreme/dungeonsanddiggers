0805fe0306f6aeb0aed8517bff840d629d47ea63
Mr. Xtreme <MisterXtreme@users.noreply.github.com>
2017-08-15 14:12:12 -0700

    Change variable names
    Changed variable names to look a bit nicer

--- dungeonsanddiggers.lua
+++ dungeonsanddiggers.lua
@@ -9,15 +9,16 @@ if answer == "y" then
   print("You awaken in a strange hallway...")
   print("You look in your pockets...")
   print("You find a fuzzy picture, a trowel, and a worn book.")
-  local whatyoudo
+  local whatGotChosen
   repeat
     io.write("Do you want to dig, look at the picture, or read the book? (1, 2, or 3)")
     io.flush()
-    whatyoudo = io.read()
-  until whatyoudo == "1" or "2" or "3"
-  if whatyoudo == "1" then
+    whatGotChosen = io.read()
+  until whatGotChosen == "1" or "2" or "3"
+  if whatGotChosen == "1" then
     print("You found a knife, it is quite large, and seems quite old, you put it in your backpack")
-  elseif whatyoudo == "2" then
+    getKnife = true
+  elseif whatGotChosen == "2" then
     print("The picture is fuzzy, but it seems familiar...")
     lookatpic = true
   end
