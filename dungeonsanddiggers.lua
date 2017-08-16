4919a5d5aaf576e7cdb0c3380b30ef3ae5751b28
Mr. Hacker <xtremetinkerer@outlook.com>
2017-08-15 21:43:52 -0700

    Start work on game mechancs
    Finish into, and begin fleshing out game mechanics

--- dungeonsanddiggers.lua
+++ dungeonsanddiggers.lua
@@ -1,3 +1,21 @@
+if not os.execute("clear") then
+    os.execute("cls")
+elseif not os.execute("cls") then
+    for i = 1,25 do
+        print("\n\n")
+    end
+end
+
+function math.randomchoice(t) --Selects a random item from a table
+    local keys = {}
+    for key, value in pairs(t) do
+        keys[#keys+1] = key --Store keys in another table
+    end
+    index = keys[math.random(1, #keys)]
+    return t[index]
+end
+
+characters = { friendly = {"Weaponsmith", "Salesman", "Wiseman"}, nuetral = {"Stranger", "Shifty Salesman"}, hostile = {"Walker", "Tank", "Archer"}}
 local answer
 local yourName
 print("Welcome to Dungeons & Diggers!")
@@ -27,25 +45,43 @@ if answer == "y" then
 
   print("Holding the items, you decide to look at the photo, it seems familiar...")
   print("You realize why, It's you!")
-  io.write("You can't seem to remember your name, so you decide to call yourself ")
+  io.write("You can't seem to remember your name, so you decide to call yourself (type your name) ")
   io.flush()
   yourName = io.read()
-  print(yourName .. " you like it, it seems perfect...")
+  print(yourName .. ", you like it, it seems perfect...")
   print("\n")
-  print("After saying " .. yourName .. "several times, you remember the other items you are holding.")
+  print("After saying " .. yourName .. " several times, you remember the other items you are holding.")
   print("You decide to read the book...")
   print("\n")
   print("My name is " .. yourName)
-  print("I don't know what happened to me, I was running down the ally, next thing I knew, I was clutching my head in pain.")
+  print("I don't know what happened to me, I was running down an ally, next thing I knew, I was clutching my head in pain.")
   print("After I took in my suroundings (all I could see was what seemed to be mossy walls, and the light from a dim torch), I realized there was someone beside me.")
-  print('When "they" (I could not tell what they looked like in the dim light, and their voice did not seem either male or female)')
-  print('saw I was awake, they told me to "Find the keeper", then they left...')
-  local continueVar
+  print('When "they" saw I was awake, they told me to "Find the keeper", then they left...')
+
+  io.write('Do you want to dig? (y/n) ')
+  io.flush()
+  local choice = io.read()
+  if choice == "y" then
+    print("You find a sword, it appears to be quite old")
+  elseif choice == "n" then
+    repeat
+    local choice = nil
+      print("Hmm, I think you should dig...")
+      io.write('Do you want to dig? (y/n) ')
+      io.flush()
+      if choice == "y" then
+        print("Finally!")
+        print("You find a sword, it appears to be quite old")
+      end
+    until choice == "y"
+  end
+  local choice = nil
   repeat
-    io.write('Press "Enter" to continue ')
+    io.write('Press "Enter" to continue')
     io.flush()
-    continueVar = io.read()
-  until continueVar ~= nil
+    choice = io.read()
+  until choice ~= nil
+
   if not os.execute("clear") then
       os.execute("cls")
   elseif not os.execute("cls") then
@@ -53,7 +89,31 @@ if answer == "y" then
           print("\n\n")
       end
   end
-  print("Hi")
+  repeat
+    local choice = nil
+    io.write('You decide to walk down the (left/right) hallway? ')
+    io.flush()
+    choice = io.read()
+  until choice == "left" or "right"
+  --if choice == "l"
+  function startRoom(direction)
+    print("You walk into the " .. direction .. " hallway...")
+    math.randomchoice(characters)
+    math.randomchoice(character)
+    print("You walk into a large room, you notice a " .. character " ...")
+    if characterType == "friendly" then
+      print("You approach the " .. chracter " ...")
+    elseif characterType == "neutral"
+    repeat
+      io.write('Do you want to approach the ' .. character .. ' (y/n)? ')
+      approach = io.read()
+    until approach == "y" or "n"
+    if
+  )
+  )
+  end
+
+
 
 elseif answer == "n" then
   print("Why did you start the game then?")
